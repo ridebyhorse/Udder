27407cb4e45a07c1775f87fca5f72bf8dc2dbb47
Maria Nesterova <ridebyhorse@gmail.com>
2024-02-17 22:30:04 +0300

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Мария Нестерова on 17.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
